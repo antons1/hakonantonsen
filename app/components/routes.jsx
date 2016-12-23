63330e93201989727e3990a7282f7aca809a1a9f
Håkon Antonsen <haakon@antons1.net>
2016-12-23 13:34:50 +0100

    Remove dependencies that are unused

--- app/components/routes.jsx
+++ app/components/routes.jsx
@@ -2,11 +2,6 @@
 
 import React from 'react';
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
-//const Router = require("react-router").Router;
-//const Route = require("react-router").Route;
-//const IndexRoute = require("react-router").IndexRoute;
-//const Link = require("react-router").Link;
-//const React = require("react");
 
 import App from './App';
 import FrontPage from './FrontPage';
