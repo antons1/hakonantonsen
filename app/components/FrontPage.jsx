29788838b5cfdcdfd2f767de0a2e0dbf4262db8d
Håkon Antonsen <haakon@antons1.net>
2016-12-23 15:30:47 +0100

    Make "English:" bold

--- app/components/FrontPage.jsx
+++ app/components/FrontPage.jsx
@@ -10,5 +10,5 @@ export default (props) =>
         <h1>H&aring;kon Antonsen</h1>
         <p>Som du kan se har jeg ikke fått gjort så veldig mye her enda, så kom tilbake senere om du er interessert i å se hva som skjer.</p>
         <p>Om du ønsker kontakt med meg kan du nå meg på haakon (at) antons1 (dot) net</p>
-        <p>English: This page is really empty as you can see, but stuff will come. If you want to contact me, you can reach me on haakon (at) antons1 (dot) net</p>
+        <p><b>English:</b> This page is really empty as you can see, but stuff will come. If you want to contact me, you can reach me on haakon (at) antons1 (dot) net</p>
     </div>;
