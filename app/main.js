9202b5f6acbcf52462de2c4dbe77a8c2382f863c
Håkon Antonsen <haakon@antons1.net>
2016-12-23 15:30:29 +0100

    Remove unused import

--- app/main.js
+++ app/main.js
@@ -2,8 +2,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import {Router, Route, Link, IndexRoute} from "react-router";
 import Routes from "./components/Routes";
 
 ReactDOM.render(<Routes />, document.getElementById('app'));
