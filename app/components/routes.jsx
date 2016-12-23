e8091cf95489e615c7f8fad53f61bfdbb8344c25
Håkon Antonsen <haakon@antons1.net>
2016-12-23 16:45:33 +0100

    Rename placeholders

--- app/components/routes.jsx
+++ app/components/routes.jsx
@@ -4,12 +4,11 @@ import React from 'react';
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 
 import App from './App';
-import FrontPage from './FrontPage';
+import Placeholder from './Placeholder';
 
 export default (props) =>
     <Router history={browserHistory}>
         <Route path="/" component={App}>
-            <IndexRoute component={FrontPage} />
-            <Route path="blog" component={FrontPage} />
+            <IndexRoute component={Placeholder} />
         </Route>
     </Router>;
