e9214b87024cbf28cc90b4fe0fdaf2e1a41cf7f1
Håkon Antonsen <haakon@antons1.net>
2016-12-24 12:14:38 +0100

    Add footer

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Header from "./shared/Header";
+import Footer from "./shared/Footer";
 
 class App extends React.Component {
     constructor() {
@@ -14,6 +15,7 @@ class App extends React.Component {
             <div className="App">
                 <Header />
                 {this.props.children}
+                <Footer />
             </div>
         );
     }
---
+++ app/components/shared/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default (props) =>
+    <footer className="container main-footer">
+        <pre className="footer-element">
+            H&aring;kon Antonsen
+        </pre>
+        <pre className="footer-element">
+            haakon(at)antons1(dot)net
+        </pre>
+    </footer>;
