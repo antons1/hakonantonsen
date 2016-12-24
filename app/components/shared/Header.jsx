a9c8f417a9b9f0406a431b8ce9779bfb250a4f13
Håkon Antonsen <haakon@antons1.net>
2016-12-24 01:02:35 +0100

    Change from rem to em

--- app/components/shared/Header.jsx
+++ app/components/shared/Header.jsx
@@ -22,14 +22,16 @@ class Header extends React.Component {
             <header className="padded container main-header">
                 <div className="titles">
                     <h1>Håkon Antonsen</h1>
-                    <h3>M. Sc student, full-stack developer</h3>
+                    <h4>M. Sc student, full-stack developer</h4>
                 </div>
-                <a className="hamburger" href="#" onClick={this.toggleMenu}></a>
+                <span className="hamburger-wrapper">
+                    <a className="hamburger" href="#" onClick={this.toggleMenu}></a>
+                </span>
                 <nav className="main-nav">
                     <ul className={this.state.visibleMenu ? "visible" : ""}>
-                        <li><Link to="/cv">CV</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/blog">Blog</Link></li>
+                        <li><h1><Link to="/cv">CV</Link></h1></li>
+                        <li><h1><Link to="/about">About</Link></h1></li>
+                        <li><h1><Link to="/blog">Blog</Link></h1></li>
                     </ul>
                 </nav>
             </header>
