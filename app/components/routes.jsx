b37dc17ba8245a61cf2f32a5c5856bcd9745b350
Håkon Antonsen <haakon@antons1.net>
2016-12-25 00:54:39 +0100

    Add temporary CV page

--- app/components/routes.jsx
+++ app/components/routes.jsx
@@ -4,11 +4,13 @@ import React from 'react';
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 
 import App from './App';
-import Placeholder from './Placeholder';
+import FrontPage from './FrontPage';
+import CV from './CV';
 
 export default (props) =>
     <Router history={browserHistory}>
         <Route path="/" component={App}>
-            <IndexRoute component={Placeholder} />
+            <IndexRoute component={FrontPage} />
+            <Route path="cv" component={CV} />
         </Route>
     </Router>;
