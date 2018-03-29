fb9365b5e3218845082e6178d7ed666572093440
Håkon Antonsen <haakon@antons1.net>
2018-03-29 21:22:30 +0200

    Fix header by using PageContentBox and updating styles to BEM

--- app/components/organisms/PageHeader/PageHeader.jsx
+++ app/components/organisms/PageHeader/PageHeader.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import { Link } from "react-router";
 import './PageHeader.less';
 
+import PageContentBox from '../PageContentBox/PageContentBox'
+
 class PageHeader extends React.Component {
     constructor() {
         super();
@@ -20,21 +22,23 @@ class PageHeader extends React.Component {
 
     render() {
         return(
-            <header className="padded background container main-header">
-                <div className="titles">
+            <header className="main-header">
+                <PageContentBox>
+                <div className="main-header__titles">
                     <h1>Håkon Antonsen</h1>
                     <h4>M. Sc student, full-stack developer</h4>
                 </div>
                 <span className="hamburger-wrapper">
-                    <a className="hamburger" href="#" onClick={this.toggleMenu}></a>
+                    <a className="hamburger-wrapper__hamburger" href="#" onClick={this.toggleMenu}></a>
                 </span>
                 <nav className="main-nav">
-                    <ul className={this.state.visibleMenu ? "visible" : ""}>
-                        <li><h1><Link to="/cv">CV</Link></h1></li>
-                        <li><h1><Link to="/about">About</Link></h1></li>
-                        <li><h1><Link to="/blog">Blog</Link></h1></li>
+                    <ul className={"main-nav__list" + (this.state.visibleMenu ? " main-nav__list--visible" : "")}>
+                        <li className="nav-list-item"><h1><Link to="/cv" className="nav-list-item__link">CV</Link></h1></li>
+                        <li className="nav-list-item"><h1><Link to="/about" className="nav-list-item__link">About</Link></h1></li>
+                        <li className="nav-list-item"><h1><Link to="/blog" className="nav-list-item__link">Blog</Link></h1></li>
                     </ul>
                 </nav>
+                </PageContentBox>
             </header>
         );
     }
