0aa93e5d670d5b24966e54645a1a4260f1005d1a
Håkon Antonsen <haakon@antons1.net>
2018-03-29 21:26:25 +0200

    Remove dependency to deleted less file in main.js

--- app/main.js
+++ app/main.js
@@ -3,6 +3,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from "./components/Routes";
-import "./style/all";
 
 ReactDOM.render(<Routes />, document.getElementById('app'));
